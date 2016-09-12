d00ac49171b7c0a8f5d4d9532812305d088e6e9a
liebesu <lichunjiang42@gmail.com>
2016-09-12 18:11:59 +0800

    update

---
+++ class_testfile.py
@@ -0,0 +1,19 @@
+import MySQLdb
+
+    
+def clamscan(file):
+    pass
+def jsscan(file):
+    pass
+def md5scan():
+    pass
+def yarascan():
+    pass
+def shellscan():
+    pass
+def sandboxscan():
+    pass
+def db_insert():
+    pass
+if __name__=="__main__":
+    clamscan
