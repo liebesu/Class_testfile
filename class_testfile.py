9a2eb59063242cae8ec38185c95f86014155bf4e
liebesu <lichunjiang42@gmail.com>
2016-09-13 10:44:25 +0800

    update

--- class_testfile.py
+++ class_testfile.py
@@ -1,11 +1,15 @@
+import os
 import MySQLdb
-
-    
+file_path=''
+def get_path():
+    for file in os.listdir(file_path):
+        whole_path=os.path.normcase(os.path.join(file_path,file))
+        
 def clamscan(file):
     pass
 def jsscan(file):
     pass
-def md5scan():
+def md5scan(file):
     pass
 def yarascan():
     pass
