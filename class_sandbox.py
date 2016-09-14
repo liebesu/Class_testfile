2f949b9974f4094032782914e09ed9652b0b4082
liebesu <lichunjiang42@gmail.com>
2016-09-14 16:45:14 +0800

    update

--- class_sandbox.py
+++ class_sandbox.py
@@ -2,19 +2,7 @@ import os
 import json
 import MySQLdb
 path='data.txt'
-def ass(signature_name):
-    a=open(path).readlines()
-    for line in a:
-        try:
-            m=line.split('    ')
-            if signature_name in m[1]:
-                print signature_name,m[0]
-                db_update(signature_name,m[0])
-                break
-                
-        except Exception as e:
-            print e
-            pass
+
 def db_check():
     
     db = MySQLdb.connect(host='localhost', db='sandbox_signatures', user='root', passwd='polydata', port=3306,
@@ -43,14 +31,6 @@ def db_check():
     db.commit()    
     cursor.close()
     db.close()
-def db_update(signature_name,filename):  
-    db1 = MySQLdb.connect(host='localhost', db='sandbox_signatures', user='root', passwd='polydata', port=3306,
-                             charset='utf8')
-    cursor1 = db1.cursor()    
-    update_sql='update signatures set file="%s" where name="%s"'% (filename,signature_name)
-    cursor1.execute(update_sql)  
-    db1.commit()    
-    cursor1.close()
-    db1.close()    
+   
 if __name__=="__main__":
     db_check()
